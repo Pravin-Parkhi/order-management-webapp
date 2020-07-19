dbd611586792f48b1ee54239da914e5d250f1d45
PRAVIN PARKHI <pravinparkhi@PRAVINs-MBP.domain.name>
2020-07-19 20:48:45 +0530

    Add brand logo and name

--- src/common/header/header.component.js
+++ src/common/header/header.component.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import BrandLogo from '../../assets/images/brand-logo.png'
+
 
 import './header.component.scss'
 
@@ -6,7 +8,8 @@ export default function Header () {
 
   return (
     <div className='header-wrapper'>
-        
+        <img src={BrandLogo} alt='Brand Logo' className='brand-logo' />
+        <p className='brand-name'>Order Manager</p>
     </div>
   )
 }
